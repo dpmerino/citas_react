12c52455f5f932bb37637568bc79732564c43965
dpmerino <diegopaul7@outlook.com>
2024-01-10 22:26:47 -0500

    easy work easy life

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
         <h1 className="font-black text-5xl text-cente md:w-2/3 mx-auto">
-            Seguimiento Pacientes {''} 
+            Seguimiento Pacientes {''} con Git Hub {''}
             <span className="text-indigo-600">Veterinaia</span>
         </h1>
 
